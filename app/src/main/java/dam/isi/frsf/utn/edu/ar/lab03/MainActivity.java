4e4545e04cb6eddd50d0e88405d12f79c4bf6c9e
emigioria <emigioria@hotmail.com>
2016-09-08 21:32:41 -0300

    Agragada pantalla main

--- app/src/main/java/dam/isi/frsf/utn/edu/ar/lab03/MainActivity.java
+++ app/src/main/java/dam/isi/frsf/utn/edu/ar/lab03/MainActivity.java
@@ -2,6 +2,8 @@ package dam.isi.frsf.utn.edu.ar.lab03;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //TODO hacer
+        return true;
     }
 }
